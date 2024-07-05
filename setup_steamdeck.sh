81ab6e8745621d0827912386567038081c42f4dd
Kazuharu Yamauchi <kazuharu@Kazuharus-MacBook-Air.local>
2024-07-05 17:04:57 +0900

    steamdeck setup shell

---
+++ setup_steamdeck.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#sudo password setup
+passwd
+
+# allow to write over file system (steam os block)
+sudo steamos-readonly disable
+
+# ensure we have the keys updated
+sudo pacman-key --init
+sudo pacman-key --populate holo
+sudo pacman-key --populate archlinux
+
+# install git and base-devel dependencies
+sudo pacman -S --needed git base-devel
+
+# install yay from binary
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+git checkout 96f90180a3cf72673b1769c23e2c74edb0293a9f
+makepkg -si
+
+# disable again the write over file system (steam os block)
+sudo steamos-readonly enable
